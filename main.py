ec43323fc2e0215a9b69685ec47ad904befdfef7
RuTiO2le <sodenururutuyunoyukaritoomou@gmail.comsodenururutuyunoyukaritoomou@gmail.com>
2023-12-11 01:49:46 +0000

    clean

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():    
-    return render_template("index2.html")
+    return render_template("index.html")
   
 @app.route("/get")
 def get_bot_response():    
-    userText = request.args.get('msg')  
-    response = get_completion(userText)  
+    userText = request.args.get('msg')
+    response = get_completion(userText)
     return response
   
 if __name__ == "__main__":
